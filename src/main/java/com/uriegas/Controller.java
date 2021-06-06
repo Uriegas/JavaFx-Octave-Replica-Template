2a53855812aedaa8b7f16c8dc0738595351cb5ee
1930526@upv.edu.mx <1930526@upv.edu.mx>
2021-06-05 20:18:42 -0500

    Handle buttons events: Simple printl

--- src/main/java/com/uriegas/Controller.java
+++ src/main/java/com/uriegas/Controller.java
@@ -1,5 +1,33 @@
 package com.uriegas;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class Controller {
-    
+    @FXML
+    private TextArea cmdString;
+    @FXML
+    private Button fileBtn;
+    @FXML
+    private Button pasteBtn;
+    @FXML
+    private Button currentDirBtn;
+
+    @FXML
+    protected void fileClicked(ActionEvent e){
+        System.out.println("You pressed the file button");
+    }
+    @FXML
+    protected void pasteClicked(ActionEvent e){
+        System.out.println("You pressed the paste button");
+    }
+    @FXML
+    protected void backClicked(ActionEvent e){
+        System.out.println("You pressed the back button");
+    }
+    @FXML
+    protected void selectDirClicked(ActionEvent e){
+        System.out.println("You pressed the selectDir button");
+    }
 }
